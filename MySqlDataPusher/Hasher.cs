269a39d9bd03894231abf43f081f9b85de576916
Maksim Chegulov <maksim.chegulov@onlyoffice.com>
2021-08-05 11:11:46 +0300

    Initial

---
+++ MySqlDataPusher/Hasher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace MySqlDataPusher
+{
+    public class Hasher
+    {
+        private const string MACHINE_KEY = "your_core_machinekey";
+        private const string PASSWORD_HASH = "fde9949f7b36de982464e77129945a6635885b7fba8725f02245577a579ff948";
+
+        public string ComputeHash(string userId)
+        {
+            var stage1 = PASSWORD_HASH + userId + MACHINE_KEY;
+
+            var stage2 = Encoding.UTF8.GetBytes(stage1);
+
+            var stage3 = SHA512.Create().ComputeHash(stage2);
+
+            return Convert.ToBase64String(stage3);
+        }
+    }
+}
---
+++ MySqlDataPusher/Program.cs
@@ -0,0 +1,57 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace MySqlDataPusher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (MySqlConnection mySqlConnection = new MySqlConnection("server=172.18.0.2;Port=3306;Protocol=TCP;database=onlyoffice;User Id=onlyoffice_user;password=onlyoffice_pass;"))
+            {
+                var hasher = new Hasher();
+
+                mySqlConnection.Open();
+
+                int index = 1;
+                for (int i = 0; i < 10; i++)
+                {
+                    var coreUserRows = new List<string>();
+                    var coreUserSecurityRows = new List<string>();
+
+                    var commandCoreUser = new StringBuilder("INSERT INTO core_user (tenant, id, username, firstname, lastname, email, last_modified) VALUES ");
+                    var commandUserSecurity = new StringBuilder("INSERT INTO core_usersecurity (tenant, userid, pwdhash) VALUES ");
+
+                    for (int j = 0; j < 10000; j++)
+                    {
+                        var user = Guid.NewGuid().ToString();
+
+                        coreUserRows.Add($"(1, '{user}', 'TestUser{index}', 'Test', 'testuser{index}@onlyoffice.com', 'User', NOW())");
+                        coreUserSecurityRows.Add($"(1, '{user}', '{hasher.ComputeHash(user)}')");
+                        index++;
+                    }
+
+                    commandCoreUser.Append(string.Join(",", coreUserRows));
+                    commandUserSecurity.Append(string.Join(",", coreUserSecurityRows));
+                    commandCoreUser.Append(";");
+                    commandUserSecurity.Append(";");
+
+                    using (MySqlCommand mySqlCommand = new MySqlCommand(commandCoreUser.ToString(), mySqlConnection))
+                    {
+                        mySqlCommand.CommandType = CommandType.Text;
+                        mySqlCommand.ExecuteNonQuery();
+                    }
+
+                    using (MySqlCommand sqlCommand = new MySqlCommand(commandUserSecurity.ToString(), mySqlConnection))
+                    {
+                        sqlCommand.CommandType = CommandType.Text;
+                        sqlCommand.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+    }
+}
